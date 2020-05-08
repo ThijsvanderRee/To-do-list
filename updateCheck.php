43ffc68e64ece99d2fe3d05e83e2853b86214d08
Unknown <99053846@mydavinci.nl>
2020-05-08 14:20:40 +0200

    Added bootstrap to some files

--- updateCheck.php
+++ updateCheck.php
@@ -12,11 +12,32 @@ $sql = "UPDATE list SET title = '$title', description = '$description' WHERE ID 
 
 if ($conn->query($sql) === TRUE) {
 ?>
-    Your changes are saved!
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+  <style media="screen">
+    body {
+      padding: 20px;
+    }
+    .list-group-item:nth-child(even){
+      background-color:#eee;
+    }
+  </style>
+</head>
+
+<div class="alert alert-success text-center">
+  Your changes are saved!
+</div>
 <?php
 } else {
   ?>
+  <div class="alert alert-warning text-center w-100">
     Oh no! Something went wrong...
+  </div>
   <?php
 }
 ?>
