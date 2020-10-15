5512707d4618f0027752c6808dd03579c35b734f
Unknown <99053846@mydavinci.nl>
2020-10-15 13:49:15 +0200

    Progression for sort function

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 function sortAsc() {
     var j = document.querySelectorAll('.card').length;
+    element = [];
+    for (let k = 0; k < j; k++) {
+        
+        var x = document.getElementsByClassName("list-group")[k].id;
+        element.push(x);
+    }
+    console.log(element);
+
     for (i = 0; i < j; i++) {
 
-        var $sortGroup = $('.sort-group-0');
-        console.log($sortGroup)
+        var $sortGroup = $(element[i]);
+        console.log(element[i]);
+        console.log($sortGroup);
     
         $sortGroup.find('.list-group-item').sort(function (a, b) {
             return +a.getAttribute('data-time') - +b.getAttribute('data-time');
         })
             .appendTo($sortGroup);
-            console.log(j);
+            // console.log(j);
     }
 }
 
-function sortDesc() {
-    var j = document.querySelectorAll('.card').length;
-    for (i = 0; i < j; i++) {
+// function getID() {
+//     var j = document.querySelectorAll('.card').length;
+//     element = [];
+//     for (let k = 0; k < j; k++) {
+        
+//         var x = document.getElementsByClassName("list-group")[k].id;
+//         element.push(x);
+//     }
+//     console.log(element);
+// }
 
-        var $sortGroup = $('.sort-group-'.i);
+// function sortDesc() {
+//     var j = document.querySelectorAll('.card').length;
+//     for (i = 0; i < j; i++) {
 
-        $sortGroup.find('.list-group-item').sort(function (b, a) {
-            return +a.getAttribute('data-time') - +b.getAttribute('data-time');
-        })
-            .appendTo($sortGroup);
-            console.log(j);
-    }
-}
+//         var $sortGroup = $('.sort-group-'.i);
+
+//         $sortGroup.find('.list-group-item').sort(function (b, a) {
+//             return +a.getAttribute('data-time') - +b.getAttribute('data-time');
+//         })
+//             .appendTo($sortGroup);
+//             console.log(j);
+//     }
+// }
